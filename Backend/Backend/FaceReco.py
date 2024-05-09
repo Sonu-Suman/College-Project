24028a8255f8a10cdb7d1722261a59858c439777
Aparajita Suman <sumankumar86028@gmail.com>
2024-05-09 16:48:13 +0530

    Now our project is fully functional. and this is now strated to working on live recorded data.

--- Backend/Backend/FaceReco.py
+++ Backend/Backend/FaceReco.py
@@ -3,7 +3,8 @@ import cv2
 import cvzone
 import numpy as np
 import face_recognition
-
+import io
+from PIL import Image
 
 
 # --------------------------- For Webcam ---------------------------------------------
@@ -19,7 +20,7 @@ import face_recognition
 # ------------------------ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX -------------------------------------
 
 class Facereco:
-    def recognization():
+    def recognization(Img):
 
         # Import the encode file from EncodeGenerator.py program file
         file = open("C:/Users/HP/React/Testing/Backend/Backend/encodelist.p", 'rb')
@@ -29,42 +30,44 @@ class Facereco:
         # print(studentIds)
 
 
-        while True:
+        # while True:
             # ------------------ For Webcam ------------------------------------------------------
             # success, img = cap.read()
             # if not success:
             #     print("Can't receive frame (stream end?). Exiting ...")
             #     break
             # -------------------  xxxxxxxxxxxxxxxxx ---------------------------------------------
-            img = cv2.imread("C:/Users/HP/React/Testing/Backend/foo.jpeg")
+            # img = cv2.imread("C:/Users/HP/React/Testing/Backend/foo.jpeg")
+        img = Image.open(io.BytesIO(Img))
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-            imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
 
-            # Our operations on the frame come here
-            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+        # Our operations on the frame come here
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-            faceCurFrame = face_recognition.face_locations(imgS)
-            encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+        faceCurFrame = face_recognition.face_locations(imgS)
+        encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
-            for encodeface, faceloc in zip(encodeCurFrame, faceCurFrame):
-                matches = face_recognition.compare_faces(encodeListKnown, encodeface)
-                faceDis = face_recognition.face_distance(encodeListKnown, encodeface)
-                # print("Face matches : ", matches, " Face Distance : ", faceDis)
-                matchId = np.argmin(faceDis)
+        for encodeface, faceloc in zip(encodeCurFrame, faceCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeface)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeface)
+            # print("Face matches : ", matches, " Face Distance : ", faceDis)
+            matchId = np.argmin(faceDis)
 
-                if matches[matchId]:
-                    y1, x2, y2, x1 = faceloc
-                    y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-                    bbox = x1, y1, x2-x1, y2-y1
-                    img = cvzone.cornerRect(img, bbox=bbox, rt=0)
-                    print("Matched student name : ", studentIds[matchId])
-                else:
-                    print("You are not registered in the database.")
+            if matches[matchId]:
+                y1, x2, y2, x1 = faceloc
+                y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                bbox = x1, y1, x2-x1, y2-y1
+                img = cvzone.cornerRect(img, bbox=bbox, rt=0)
+                print("Matched student name : ", studentIds[matchId])
+            else:
+                print("You are not registered in the database.")
 
 
-            cv2.imshow("Face Recognition system", img)
-            if cv2.waitKey(1) == ord('q'):
-                break
+        cv2.imshow("Face Recognition system", img)
+        # if cv2.waitKey(1) == ord('q'):
+        #     break
 
 
 # if __name__ == "__main__":
